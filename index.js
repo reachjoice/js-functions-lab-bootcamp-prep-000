2cf97a00d0cea6760bdcbfc4876c41b7cff3db5b
Joice Pilli <reach.joice@gmail.com>
2017-10-26 16:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-return 'Happy holidays, ${name}!'  
+return 'Happy holidays, ${name}!'
 }
+function happyHolidaysTo(name){
+return 'Happy holidays, ${name}!'
+}
+function happyHolidayTo(holiday,name){
+return 'Happy ${holiday}, ${name}!'
+}
+function happyCountdown(holiday,days){
+return 'It\'s ${days} days until ${holiday}!'
+}
+
